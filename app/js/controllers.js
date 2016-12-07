0877470b28456651c88d4ec8dc30ef9f31f710bc
Sara Ahlers <sjahlers@gmail.com>
2016-12-07 14:28:51 -0500

    added code to retrieveCountryData function to get the API data and alert an error for invalid country name

--- app/js/controllers.js
+++ app/js/controllers.js
@@ -1,27 +1,24 @@
 (function (){
 var app = angular.module('tripSnipModule');
 
-app.controller("snipCreatorCtrl", function ($scope){
+app.controller("snipCreatorCtrl", function ($scope, $http){
+	//retrieve user-entered data
 	$scope.retrieveCountryData = function(){
 		console.log($scope.countrySearch);
 		console.log($scope.note);
 
+	//retrieve API data for user-entered country
+	$http.get("https://restcountries.eu/rest/v1/name/" + $scope.countrySearch + "?fullText=true").
+			then(function(response) {
+				$scope.countryData = response.data;
+				console.log($scope.countryData[0].nativeName);
+			}, function errorCallback(response){
+				alert("Please check the country spelling");
+			});	
 
 	};
 
 
-
-
-
-
-
-//  $scope.snippetData = [
-//    {
-//      country: "Spain",
-//      nativeName: "Espana",
-//      population: 2
-//    }
-//  ];
 
 });
 
