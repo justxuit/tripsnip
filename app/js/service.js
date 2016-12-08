7a3b41c6c5def53b7b1fa48449761d796148f364
Xu <email.xhuang@gmail.com>
2016-12-08 13:23:19 -0500

    Started formatting on snips.html and snip-template.html

--- app/js/service.js
+++ app/js/service.js
@@ -26,6 +26,48 @@ app.factory("snippetStorage", function(){ //creates factory
       capital: "Phnom Penh",
       population: 15405157,
       note: "No McDonald's here! What's the deal?!? :("
+    },
+    {
+      name: "Cambodia",
+      nativeName: "Kâmpŭchéa",
+      capital: "Phnom Penh",
+      population: 15405157,
+      note: "No McDonald's here! What's the deal?!? :("
+    },
+    {
+      name: "Cambodia",
+      nativeName: "Kâmpŭchéa",
+      capital: "Phnom Penh",
+      population: 15405157,
+      note: "No McDonald's here! What's the deal?!? :("
+    },
+    {
+      name: "Cambodia",
+      nativeName: "Kâmpŭchéa",
+      capital: "Phnom Penh",
+      population: 15405157,
+      note: "No McDonald's here! What's the deal?!? :("
+    },
+    {
+      name: "Cambodia",
+      nativeName: "Kâmpŭchéa",
+      capital: "Phnom Penh",
+      population: 15405157,
+      note: "No McDonald's here! What's the deal?!? :("
+    },
+    {
+      name: "Cambodia",
+      nativeName: "Kâmpŭchéa",
+      capital: "Phnom Penh",
+      population: 15405157,
+      note: "No McDonald's here! What's the deal?!? :("
+    },
+    {
+      name: "Cambodia",
+      nativeName: "Kâmpŭchéa",
+      capital: "Phnom Penh",
+      population: 15405157,
+      note: "No McDonald's here! What's the deal?!? :("
     }
   ];
 
