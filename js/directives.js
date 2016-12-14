5e48e871d926d65f3752a57021ad4e5947772dca
Xu <email.xhuang@gmail.com>
2016-12-14 01:29:46 -0500

    Revert "Reverted .git location"
    This reverts commit c0522726dd78b7bd0fc288dab1a00ed2a40ccb26.

---
+++ js/directives.js
@@ -0,0 +1,1 @@
+!function(){var app=angular.module("tripSnipModule");app.directive("snipCreator",function(){return{restrict:"E",templateUrl:"./partials/snip-template.html"}}),app.directive("autoComplete",function($timeout){return function(scope,iElement,iAttrs){iElement.autocomplete({source:scope[iAttrs.uiItems],select:function(){$timeout(function(){iElement.trigger("input")},0)}})}})}();
